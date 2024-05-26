aeb275c071501a664916b94e69713c9f904ae10b
RichardoRNZ <richardowijaya772@gmail.com>
2024-05-26 19:04:14 +0700

    final version

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            CustomerSeeder::class,
-            ProductSeeder::class,
+            // CustomerSeeder::class,
+            // ProductSeeder::class,
 
         ]);
     }
